d410d1af4cf6862651291331feaddc5a7f6d3d75
Shlomi Fish <shlomif@shlomifish.org>
2013-01-18 23:16:28 +0200

    Minimum version.

--- XML-Grammar-Vered/lib/XML/Grammar/Vered.pm
+++ XML-Grammar-Vered/lib/XML/Grammar/Vered.pm
@@ -7,8 +7,8 @@ use autodie;
 
 use MooX qw/late/;
 
-use XML::GrammarBase::Role::RelaxNG;
-use XML::GrammarBase::Role::XSLT;
+use XML::GrammarBase::Role::RelaxNG v0.1.5;
+use XML::GrammarBase::Role::XSLT v0.1.5;
 
 our $VERSION = '0.0.1';
 
